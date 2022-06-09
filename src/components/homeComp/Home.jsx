cf35ac73d4d1c41a795c584d8518927021acf601
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-09 09:57:54 +0200

    parallax

--- src/components/homeComp/Home.jsx
+++ src/components/homeComp/Home.jsx
@@ -1,7 +1,10 @@
 import Styles from "./home.module.css";
 import { PaperAirplaneIcon } from "@heroicons/react/solid";
+import { Parallax } from "react-parallax";
 import goonies from "../../img/goonies.png";
 import shipsite from "../../img/shipsite.png";
+const image1 =
+  "https://images.unsplash.com/photo-1654707981578-e1a5ad419ea0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80";
 
 const Home = () => {
   return (
@@ -16,7 +19,12 @@ const Home = () => {
                 Very smal, but playful website, with shitloads of stuff going
                 on, but a little bit anoying to acctualy use.
               </p>
-              <img className={Styles.img} src={goonies} alt='' />
+              <Parallax
+                bgImage={goonies}
+                style={{ marginTop: "2vh", border: "solid 1px white " }}
+              >
+                <div style={{ width: "30vw", height: "35vh" }}></div>
+              </Parallax>
               <p className={Styles.textCursive}>
                 Havent dove into who made the website or with wich libarary,
                 just liked the page and added it.
@@ -28,11 +36,19 @@ const Home = () => {
               <h3 className={Styles.title}>How Good can it get</h3>
               <p className={Styles.text}>
                 I heard a story about 3 backend developers whom had a idea that
-                they were gonna start a website to find sunken shipwrecks. I
-                went out for a search and found it, but never in my mind could i
-                imagine the beauty they had been able to create.
+                they were gonna start a website to find sunken shipwrecks.
               </p>
-              <img className={Styles.img} src={shipsite} alt='' />
+              <Parallax
+                bgImage={shipsite}
+                style={{ marginTop: "2vh", border: "solid 1px white " }}
+              >
+                <div style={{ width: "30vw", height: "35vh" }}></div>
+              </Parallax>
+              <p className={Styles.text}>
+                I went out for a search and found it, but never in my mind could
+                i imagine the beauty they had been able to create.
+              </p>
+
               <p className={Styles.textCursive}>Love them backenders</p>
             </a>
           </section>
@@ -100,6 +116,15 @@ const Home = () => {
               BSTD
             </a>
           </section>
+        </div>
+        <div style={{ width: "100vw", height: "30vh" }}>
+          <Parallax
+            blur={{ min: -15, max: 15 }}
+            bgImage={image1}
+            style={{ width: "100vw", height: "30vh", marginTop: "20vh" }}
+          >
+            <div style={{ width: "100vw" }}></div>
+          </Parallax>
         </div>
       </div>
       <PaperAirplaneIcon className={Styles.plane} />
