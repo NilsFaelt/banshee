399857b264cc9452d975248675ae81ffcc0da739
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-15 09:06:22 +0200

    footer done

--- src/components/displayComponents/footers/Footers.jsx
+++ src/components/displayComponents/footers/Footers.jsx
@@ -7,7 +7,7 @@ const Footers = () => {
     <div className={Styles.container}>
       <h2 className={Styles.title}>Footers</h2>
       <CombLibNav />
-      <h3 className={Styles.footersTitle}>Click the cog</h3>
+      <h3 className={Styles.footersTitle}>Click the cogs</h3>
       <FooterOne />
     </div>
   );
